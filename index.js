91e9443888f3c1ff0e33c9c37ebacdb898798045
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-09-07 13:51:34 +0100

    db fix

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const server = http.createServer(app)
 const { API_PORT} = process.env
 const port = process.env.PORT || API_PORT 
 
+
+if (process.env.NODE__ENV === 'production') {
+    
+}
 server.listen(port, () => {
     console.log(`server is listening at port ${port}`)
 })
